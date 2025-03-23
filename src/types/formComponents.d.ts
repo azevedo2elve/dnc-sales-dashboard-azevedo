110ac42a0e9f13806b4b0acfa2ddacecfbfc9a39
gabrielazevedo-egsys <gabriel.silva@egsys.com.br>
2025-03-23 20:39:41 -0300

    Feat: Form components

--- src/types/formComponents.d.ts
+++ src/types/formComponents.d.ts
@@ -1,5 +1,5 @@
 export type InputProps = React.InputHTMLAttributes<HTMLInputElement>
-type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement>
+export type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement>
 
 export type MessageProps = {
   msg: string
@@ -8,6 +8,6 @@ export type MessageProps = {
 
 export interface FormComponentProps {
   inputs: InputProps[]
-  button: ButtonProps[]
+  buttons: ButtonProps[]
   message?: MessageProps
 }
