95fa28d7bd2c02ad60826cfc04fd033fcd09a550
azevedo2elve <azevedogabriel00@gmail.com>
2025-04-12 22:05:02 -0300

    Feat: Profile Request

--- src/hooks/useFormValidation.ts
+++ src/hooks/useFormValidation.ts
@@ -9,6 +9,10 @@ export const useFormValidation = (inputs: InputProps[]) => {
 
   useEffect(() => {
     const allFieldsValid = inputs.every((inputs, index) => {
+      const value = formValues[index]
+      if (inputs.required && !value) {
+        return false
+      }
       if (inputs.type === 'email') {
         return /\S+@\S+\.\S+/.test(String(formValues[index]))
       }
